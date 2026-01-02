584c7d839075236e20a5b3807a8054c3a2923ea9
Abdurezak Akmel <abduakmel16@gmail.com>
2026-01-02 11:08:28 +0300

    new

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,4 +1,28 @@
-export const authService = {
-  login: async () => {},
-  register: async () => {},
+const API_URL = "http://localhost:5000/api/auth"; // backend URL
+
+export const registerUser = async (data: {
+  email: string;
+  password: string;
+  role: "EMPLOYEE" | "ADMIN";
+}) => {
+  const res = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  return res.json();
+};
+
+export const loginUser = async (data: {
+  email: string;
+  password: string;
+}) => {
+  const res = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  return res.json();
 };
